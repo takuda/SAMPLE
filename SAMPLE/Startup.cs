de62f6b1e211ed24ba1269e1bce993628727a2f4
takuda <takuda.n@proas.co.jp>
2017-10-29 03:12:11 +0900

    dev_userA commit1

---
+++ SAMPLE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAMPLE.Startup))]
+namespace SAMPLE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
